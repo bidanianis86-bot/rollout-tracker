b12ae668d3a1e58cb336480ad0f7bef562442a83
Anis <bidani.anis86@gmail.com>
2026-07-06 17:52:12 +0300

    debug: add diagnostic overlay to trace init failure

--- push.bat
+++ push.bat
@@ -1,13 +1,15 @@
 @echo off
 cd /d "D:\claude\Projects\Rollout projects\tracker"
 
-:: Remove stale git lock if present
+:: Remove stale git lock files
 if exist ".git\index.lock" del /f ".git\index.lock"
+if exist ".git\HEAD.lock" del /f ".git\HEAD.lock"
+if exist ".git\COMMIT_EDITMSG.lock" del /f ".git\COMMIT_EDITMSG.lock"
 
 git config user.email "bidani.anis86@gmail.com"
 git config user.name "Anis"
-git add index.html .github/workflows/deploy.yml
-git commit -m "fix: renderOgSites broken onchange handlers causing app crash - use data-sid attributes"
+git add index.html .github/workflows/deploy.yml push.bat
+git commit -m "debug: add diagnostic overlay to trace init failure" --allow-empty
 git push origin main
 echo.
 echo Done! Site will update in ~1 minute.
